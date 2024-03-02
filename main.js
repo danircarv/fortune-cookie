95373fa82d308b285b719ac81fce3a8afd2de867
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-02 07:34:40 -0300

    adjuting the functionality

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-const arr = ["1", "2", "3", "4", "5"];
+const arr = ["", "2", "3"];
 const randomIndex = Math.floor(Math.random() * arr.length);
 const randomElement = arr[randomIndex];
-const imgShow = `phrase${randomElement}`;
 
-function handleClick(event) {
-  event.preventDefault();
-
+function handleShowClick() {
   document.querySelector(".screen1").classList.toggle("hide");
   document.querySelector(".screen2").classList.toggle("hide");
-  document.querySelector(`.phrase${randomElement}`).toggle("hide");
+  const img = document.querySelector(".phrase");
+  img.src = `./assets/sorte${randomElement}.png`;
 }
+
+function newCookie() {
+  document.querySelector(".screen1").classList.toggle("hide");
+  document.querySelector(".screen2").classList.toggle("hide");
+}
+
+//Eventos
+const cookie = document.querySelector("#cookie");
+const btnReset = document.querySelector("#btnReset");
+
+cookie.addEventListener("click", handleShowClick);
+btnReset.addEventListener("click", newCookie);
