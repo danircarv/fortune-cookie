ef6da4c625abb2076f73c95c0fc2b18cf549a166
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-02 07:44:34 -0300

    simplifying the variables

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+
 const arr = ["", "2", "3"];
 const randomIndex = Math.floor(Math.random() * arr.length);
 const randomElement = arr[randomIndex];
 
 function handleShowClick() {
-  document.querySelector(".screen1").classList.toggle("hide");
-  document.querySelector(".screen2").classList.toggle("hide");
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
   const img = document.querySelector(".phrase");
   img.src = `./assets/sorte${randomElement}.png`;
 }
 
 function newCookie() {
-  document.querySelector(".screen1").classList.toggle("hide");
-  document.querySelector(".screen2").classList.toggle("hide");
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
 }
 
 //Eventos
